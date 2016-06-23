63a75039d039258b497462960c42a3e99e1acf81
francesco2013 <ffazio1@gmail.com>
2016-06-23 10:32:40 +0200

    Add files via upload

---
+++ include/class.myconfig.php
@@ -0,0 +1,13 @@
+class MyConfig
+{
+    public static function read($filename)
+    {
+        $config = include $filename;
+        return $config;
+    }
+    public static function write($filename, array $config)
+    {
+        $config = var_export($config, true);
+        file_put_contents($filename, "<?php return $config ;");
+    }
+}
---
+++ include/ip2locationlite.class.php
@@ -0,0 +1,59 @@
+<?php
+final class ip2location_lite{
+	protected $errors = array();
+	protected $service = 'api.ipinfodb.com';
+	protected $version = 'v3';
+	protected $apiKey = '';
+
+	public function __construct(){}
+
+	public function __destruct(){}
+
+	public function setKey($key){
+		if(!empty($key)) $this->apiKey = $key;
+	}
+
+	public function getError(){
+		return implode("\n", $this->errors);
+	}
+
+	public function getCountry($host){
+		return $this->getResult($host, 'ip-country');
+	}
+
+	public function getCity($host){
+		return $this->getResult($host, 'ip-city');
+	}
+
+	private function getResult($host, $name){
+		$ip = @gethostbyname($host);
+
+		// if(filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_IPV6)){
+		if(filter_var($ip, FILTER_VALIDATE_IP)){
+			$xml = @file_get_contents('http://' . $this->service . '/' . $this->version . '/' . $name . '/?key=' . $this->apiKey . '&ip=' . $ip . '&format=xml');
+
+
+			if (get_magic_quotes_runtime()){
+				$xml = stripslashes($xml);
+			}
+
+			try{
+				$response = @new SimpleXMLElement($xml);
+
+				foreach($response as $field=>$value){
+					$result[(string)$field] = (string)$value;
+				}
+
+				return $result;
+			}
+			catch(Exception $e){
+				$this->errors[] = $e->getMessage();
+				return;
+			}
+		}
+
+		$this->errors[] = '"' . $host . '" is not a valid IP address or hostname.';
+		return;
+	}
+}
+?>
