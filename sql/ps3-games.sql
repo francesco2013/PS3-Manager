69b6178522753a025ce5a82b1497cae985ba726a
francesco2013 <ffazio1@gmail.com>
2016-08-02 02:44:36 +0200

    Update ps3-games.sql

--- sql/ps3-games.sql
+++ sql/ps3-games.sql
@@ -1,28 +1,50 @@
--- MySQL dump 10.13  Distrib 5.5.47, for debian-linux-gnu (x86_64)
+-- phpMyAdmin SQL Dump
+-- version 3.4.11.1deb2+deb7u5
+-- http://www.phpmyadmin.net
 --
--- Host: localhost    Database: ps3-games
--- ------------------------------------------------------
--- Server version	5.5.47-0+deb7u1
+-- Host: localhost
+-- Generation Time: Aug 02, 2016 at 12:40 AM
+-- Server version: 5.5.49
+-- PHP Version: 5.6.23-1~dotdeb+7.1
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Database: `ps3-games`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `games`
+--
+
+CREATE TABLE IF NOT EXISTS `games` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `lastplayed` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `time_played` int(11) NOT NULL,
+  `dateadded` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `name` text NOT NULL,
+  `isoname` text NOT NULL,
+  `covername` text NOT NULL,
+  `numplayed` int(11) NOT NULL,
+  KEY `id` (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=669 ;
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `game_details`
 --
 
-DROP TABLE IF EXISTS `game_details`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `game_details` (
+CREATE TABLE IF NOT EXISTS `game_details` (
   `id` int(11) NOT NULL,
   `name` text NOT NULL,
   `category` text NOT NULL,
@@ -35,35 +57,7 @@ CREATE TABLE `game_details` (
   `tags` text NOT NULL,
   UNIQUE KEY `id` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Table structure for table `games`
---
-
-DROP TABLE IF EXISTS `games`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `games` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `lastplayed` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `time_played` int(11) NOT NULL,
-  `dateadded` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `name` text NOT NULL,
-  `isoname` text NOT NULL,
-  `covername` text NOT NULL,
-  `numplayed` int(11) NOT NULL,
-  KEY `id` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=609 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2016-06-22 22:28:27
