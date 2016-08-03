0bb780deeae9d87a6e91bb11ef936ef860bd86d5
francesco2013 <ffazio1@gmail.com>
2016-08-04 00:29:55 +0200

    Update mysql_conf.php

--- mysql_conf.php
+++ mysql_conf.php
@@ -4,6 +4,8 @@ $mysql_host = "MYSQL_HOST";
 $mysql_user = "MYSQL_USER";
 $mysql_password = "MYSQL_PASSWORD";
 $mysql_db = "ps3-games"; // <-- Change this only if uou want to change the name of the database
+
+// DONT CHANGE ANYTHING HERE
 define('DB_USER', $mysql_user);
 define('DB_PASSWORD', $mysql_password);
 define('DB_SERVER', $mysql_host);
