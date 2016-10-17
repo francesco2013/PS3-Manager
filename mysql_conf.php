35594dbb138c68126cfe6a497d8072b5f729341d
francesco2013 <ffazio1@gmail.com>
2016-10-17 23:55:30 +0200

    Update mysql_conf.php

--- mysql_conf.php
+++ mysql_conf.php
@@ -1,10 +1,10 @@
 <?php
 
 // MYSQL Server Details
-$mysql_host = "127.0.0.1";
-$mysql_user = "root";
-$mysql_password = "Nov01ibm";
-$mysql_db = "ps3-games";
+$mysql_host = "MYSQL_SERVER_IP";
+$mysql_user = "MYSQL_USER";
+$mysql_password = "MYSQL_PASSWORD";
+$mysql_db = "ps3-games"; <---- change this only if you want to call the database in a different way
 
 define('DB_USER', $mysql_user);
     define('DB_PASSWORD', $mysql_password);
